972571e5d0e88b7166ff6285c547180e0993903e
Domingo Ruiz Arroyo <ordenadordomi@gmail.com>
2025-07-27 18:05:03 +0200

    Cambiar menu a barra superior

--- app/Providers/Filament/AsogesPanelProvider.php
+++ app/Providers/Filament/AsogesPanelProvider.php
@@ -34,6 +34,9 @@ class AsogesPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->sidebarCollapsibleOnDesktop(false)
+            ->sidebarFullyCollapsibleOnDesktop(false)
+            ->topNavigation() 
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
