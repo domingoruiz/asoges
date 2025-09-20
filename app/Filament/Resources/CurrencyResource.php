6f9b8b2cb7af2d418a675b34341751a0542646a0
Domingo Ruiz Arroyo <ordenadordomi@gmail.com>
2025-09-20 23:02:27 +0200

    Error en mantenimiento de monedas

--- app/Filament/Resources/CurrencyResource.php
+++ app/Filament/Resources/CurrencyResource.php
@@ -3,16 +3,19 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\CurrencyResource\Pages;
-use App\Filament\Resources\CurrencyResource\RelationManagers;
 use App\Models\Currency;
+use App\Exports\CurrenciesExport;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\TextInput;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
-use Filament\Forms\Components\TextInput;
-use App\Exports\CurrenciesExport;
-use Maatwebsite\Excel\Facades\Excel;
+use Filament\Tables;
 use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Facades\Excel;
 
 class CurrencyResource extends Resource
 {
@@ -40,23 +43,10 @@ class CurrencyResource extends Resource
     public static function form(Form $form): Form
     {
         return $form->schema([
-            TextInput::make('codigo_iso')
-                ->label('Código ISO')
-                ->maxLength(3)
-                ->required(),
-
-            TextInput::make('nombre')
-                ->label('Nombre')
-                ->required(),
-
-            TextInput::make('nombre_en')
-                ->label('Nombre en inglés')
-                ->required(),
-
-            TextInput::make('simbolo')
-                ->label('Simbolo')
-                ->maxLength(5)
-                ->required()
+            TextInput::make('codigo_iso')->label('Código ISO')->maxLength(3)->required(),
+            TextInput::make('nombre')->label('Nombre')->required(),
+            TextInput::make('nombre_en')->label('Nombre en inglés')->required(),
+            TextInput::make('simbolo')->label('Símbolo')->maxLength(5)->required(),
         ]);
     }
 
@@ -69,30 +59,33 @@ class CurrencyResource extends Resource
                 TextColumn::make('simbolo'),
             ])
             ->bulkActions([
-                BulkAction::make('exportar')
-                    ->label('Exportar seleccionados')
-                    ->icon('heroicon-o-arrow-down-tray')
-                    ->action(function ($records) {
-                        $ids = $records->pluck('id');
-                        return Excel::download(new CurrenciesExport($ids), 'monedas.xlsx');
-                    }),
-                Tables\Actions\DeleteBulkAction::make()
+                BulkActionGroup::make([
+                    BulkAction::make('exportar')
+                        ->label('Exportar seleccionados')
+                        ->icon('heroicon-o-arrow-down-tray')
+                        ->action(function (Collection $records) {
+                            return Excel::download(
+                                new CurrenciesExport($records->pluck('id')),
+                                'monedas.xlsx'
+                            );
+                        }),
+
+                    DeleteBulkAction::make(),
+                ]),
             ]);
     }
 
     public static function getRelations(): array
     {
-        return [
-            //
-        ];
+        return [];
     }
 
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListCurrencies::route('/'),
+            'index'  => Pages\ListCurrencies::route('/'),
             'create' => Pages\CreateCurrency::route('/create'),
-            'edit' => Pages\EditCurrency::route('/{record}/edit'),
+            'edit'   => Pages\EditCurrency::route('/{record}/edit'),
         ];
     }
 }
