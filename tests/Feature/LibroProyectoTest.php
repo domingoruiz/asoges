2be36f09196b12ab5c453df1eb08b1181b557615
Domingo Ruiz Arroyo <ordenadordomi@gmail.com>
2026-02-03 19:51:18 +0100

    Change project state from 'Estado' to 'pendiente'

--- tests/Feature/LibroProyectoTest.php
+++ tests/Feature/LibroProyectoTest.php
@@ -14,7 +14,7 @@ class LibroProyectoTest extends TestCase
         $aso = Aso::create(['nombre' => 'Asociación Proyecto Test', 'cif' => 'A00000009', 'domicilio_social' => 'Dirección Test', 'fch_constitucion' => now(), 'alt_usr' => 1]);
 
         // Crea un proyecto
-        $proyecto = LibroProyecto::create(['aso_id' => $aso->id, 'nombre' => 'Proyecto de Innovación', 'estado' => 'Pendiente', 'fecha_inicio' => now()->toDateString(), 'fecha_fin' => now()->addMonths(6)->toDateString(), 'observaciones' => 'Proyecto de prueba en el libro de proyectos.', 'alt_usr' => 1]);
+        $proyecto = LibroProyecto::create(['aso_id' => $aso->id, 'nombre' => 'Proyecto de Innovación', 'estado' => 'pendiente', 'fecha_inicio' => now()->toDateString(), 'fecha_fin' => now()->addMonths(6)->toDateString(), 'observaciones' => 'Proyecto de prueba en el libro de proyectos.', 'alt_usr' => 1]);
         $this->assertDatabaseHas('libro_proyectos', [
             'aso_id' => $aso->id,
             'nombre' => 'Proyecto de Innovación',
